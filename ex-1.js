8b1f163a5a56cacac62a44874fb5b5d2ff8cff22
FiveTW87 <thanwalokwattanakul@gmail.com>
2023-12-06 15:31:46 +0700

    feat complet Ex

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,23 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(operation(array[i]));
+  }
+  console.log(result)
+  return result;
+}
+function calNewEmpSal(valure) {
+  return valure + 5000;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+for (let i = 0; i < forEach(employeeSalaries, calNewEmpSal).length; i++) {
+  newEmployeeSalaries.push(forEach(employeeSalaries, calNewEmpSal)[i]);
+}
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,19 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  if (operation(array) >= 5) {
+    return true;
+  } else return false;
+}
+function scoreThan70(array) {
+  let than70 = 0;
+  for (let i = 0; (i < array.length); i++) {
+    if (array[i] >= 70) {
+      than70++;
+    }
+  }
+  console.log(than70)
+  return than70
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +21,10 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, scoreThan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, scoreThan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, scoreThan70);
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
