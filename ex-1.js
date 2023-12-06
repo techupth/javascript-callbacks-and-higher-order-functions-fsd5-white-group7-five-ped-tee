a7f791652ef991004a5009b0aad7b2c29902aec5
pednaruemol <naruemol.kphet@gmail.com>
2023-12-06 14:58:54 +0700

    finished:exercise1-2/Callback and  Higher order

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
+//สมมุติว่าบริษัทแห่งหนึ่งจะเพิ่มเงินเดือนให้กับพนักงานทุกคน 5000 บาทจากฐานเงินเดือนเดิมที่มีอยู่แล้ว โดยโจทย์กำหนดให้
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
+// // Using `forEach` function here
+function forEach(array, operation) {
+  for (let i = 0; i < array.length; i++) {
+    const result = operation(array[i]);
+    newEmployeeSalaries.push(result);
+  }
+  return newEmployeeSalaries;
+}
 
-// Using `forEach` function here
-
+forEach(employeeSalaries, function (addSalary) {
+  return addSalary + 5000;
+});
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,25 @@
 //Exercise #2: At Least Five Function
+// สมมุติว่าโรงเรียนแห่งหนึ่งมีเป้าหมายที่จะสอบวัดระดับวิชาวิทยาศาสตร์คอมพิวเตอร์กับนักเรียนชั้นมัธยมศึกษาปีที่ 2 โดยแต่ละห้องมีคะแนนเป็นดังนี้
+// ถ้า Return false คือหมายความว่า Array อันนี้มี Element น้อยกว่า 5 ตัวที่ตรงตามเงื่อนไขที่กำหนด
 
+// ***check >70 เกิน 5 คน
 function atLeastFive(array, operation) {
-  // Start coding here
+  if (operation(array) >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//callback function here
+function checkScoreMoreThan70(arrayScore) {
+  let countMoreThan70 = 0;
+  for (let i = 0; i < arrayScore.length; i++) {
+    if (arrayScore[i] > 70) {
+      countMoreThan70++;
+    }
+  }
+
+  return countMoreThan70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +28,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScoreMoreThan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScoreMoreThan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScoreMoreThan70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
